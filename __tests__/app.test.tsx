193368534fc8cb312f55ecb125e449190bf8d090
Richard Bidin <richardbidin@outlook.com>
2024-01-27 13:36:34 -0300

    Removes tests

--- __tests__/app.test.tsx
+++
@@ -1,9 +0,0 @@
-import { App } from '../src/app';
-import { it } from '@jest/globals';
-import React from 'react';
-import 'react-native';
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-	renderer.create(<App />);
-});
